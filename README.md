dfae5896f0da402b4684aca20a1d5e5d76c88518
Sceat <sceat@hydre.io>
2020-04-18 02:48:57 +0200

    reduce to 30lines

--- README.md
+++ README.md
@@ -2,13 +2,15 @@
 <p align=center>
   <img src="https://img.shields.io/github/license/Sidy-group/disco-pod.svg?style=for-the-badge" />
   <a href="https://www.npmjs.com/package/@sidy/gql-ws-server">
-    <img src="https://img.shields.io/docker/pulls/sidy/disco-pod?logo=docker&style=for-the-badge" />
+    <img src="https://img.shields.io/docker/pulls/insidy/disco-pod?logo=docker&style=for-the-badge" />
   </a>
-  <img src="https://img.shields.io/github/workflow/status/Sidy-group/disco-pod/CI?logo=Github&style=for-the-badge" />
+  <img src="https://img.shields.io/github/workflow/status/insidy/disco-pod/CI?logo=Github&style=for-the-badge" />
 </p>
 
 <h3 align=center>A pod discovery sidecar for Kubernetes</h3>
 <img align=center src="https://i.imgur.com/4en3yrg.png" />
+
+This lib is **very** light (30 lines of simple javascript)
 
 ## Installation
 
--- src/index.js
+++ src/index.js
@@ -3,38 +3,27 @@ import Kubernetes from 'kubernetes-client'
 import zmq from 'zeromq'
 
 const log = debug('disco-pod')
-
 const { REQUEST_PORT = 5454, SUBSCRIBE_PORT = 4545, NAMESPACE = 'default', ENDPOINT_NAME = 'disco-pod' } = process.env
 const zmq_publisher = new zmq.Publisher()
 const zmq_reply = new zmq.Reply()
-
 const k8s = new Kubernetes.Client1_13({ version: '1.13' })
-
-const fetch_endpoints = async () => k8s.api.v1.namespaces(NAMESPACE).endpoints(ENDPOINT_NAME).get()
-const subscribe_to_stream = async () => k8s.api.v1.watch.namespaces(NAMESPACE).endpoints(ENDPOINT_NAME).getObjectStream()
 
 zmq_reply.bind(`tcp://0.0.0.0:${REQUEST_PORT}`)
 zmq_publisher.bind(`tcp://0.0.0.0:${SUBSCRIBE_PORT}`)
 
 const respond_with_endpoints = async () => {
   log('fetching endpoints..')
-  const endpoints = await fetch_endpoints()
+  const endpoints = await k8s.api.v1.namespaces(NAMESPACE).endpoints(ENDPOINT_NAME).get()
   log('found %O', endpoints)
   await zmq_reply.send(endpoints)
 }
-
 const publish_update = async update => {
   log('sending update %O', update)
   await zmq_publisher.send(update)
 }
-
 const run = async () => {
-  const stream = await subscribe_to_stream()
+  const stream = await k8s.api.v1.watch.namespaces(NAMESPACE).endpoints(ENDPOINT_NAME).getObjectStream()
   stream.on('data', publish_update)
-  for (; ;) {
-    await zmq_reply.receive()
-    await respond_with_endpoints()
-  }
+  while (await zmq_reply.receive()) await respond_with_endpoints()
 }
-
 run()
